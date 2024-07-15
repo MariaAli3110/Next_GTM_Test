05f3a3736b3e05c8ef6ce060ab11f8fad2a68a53
Maria Ali <mariaali.3110@gmail.com>
2024-07-15 15:09:53 +0500

    fire tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,46 @@
 'use client'
 import { sendGTMEvent, sendGAEvent } from '@next/third-parties/google'
-
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
  
 export default function Home() {
+  const GTM_ID = 'GTM-W9FPQCL8';
+  const initializeGTM = () => {
+    TagManager.initialize({ gtmId: GTM_ID });
+  };
+
+  const trackPageView = (url: string) => {
+    TagManager.dataLayer({
+      dataLayer: {
+        event: 'pageview',
+        page: url
+      }
+    });
+  };
+  
+  const trackEvent = (eventName:string, eventProps: any) => {
+    TagManager.dataLayer({
+      dataLayer: {
+        event: eventName,
+        ...eventProps
+      }
+    });
+  };
+  
+  useEffect(() => {
+    initializeGTM();
+    trackPageView('/home');
+  }, []);
+
+  const clickHandler = () => {
+    sendGTMEvent({ event: 'buttonClicked', value: 'xyz' }); 
+    trackEvent('button_click', { category: 'Navigation', label: 'Homepage' });
+  }
+
   return (
   <div>
   <button
-        onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })}
+        onClick={clickHandler}
       >
         Send Event GTM
       </button>
